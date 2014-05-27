c463fffa670b58ba34b274ce76402cb7fb3cb945
rich-iannone <riannone@me.com>
2014-05-26 23:34:48 -0700

    Add reactions for 2-propanol

--- MCM_reactions_rates.R
+++ MCM_reactions_rates.R
@@ -17,3 +17,7 @@ C2H5OH_OH__HOCH2CH2O2 <- "3.0E-12 * exp(20/temp) * 0.05"
 NPROPOL_OH__C2H5CHO_HO2 <- "4.6E-12 * exp(70/temp) * 0.494"
 NPROPOL_OH__C2H5CHO_HO1C3O2 <- "4.6E-12 * exp(70/temp) * 0.063" 
 NPROPOL_OH__C2H5CHO_HYPROPO2 <- "4.6E-12 * exp(70/temp) * 0.443"
+
+# 2-Propanol (i-propanol)
+IPROPOL_OH__CH3COCH3_HO2 <- "2.6E-12 * exp(200/temp) * 0.86"
+IPROPOL_OH__IPROPOLO2 <- "2.6E-12 * exp(200/temp) * 0.139"
